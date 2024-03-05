6227059639d5b52d4cc3193483d465270c561e41
sachin <sachinjaing494@gmail.com>
2024-03-04 22:07:57 -0600

    crud

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express= require("express");
 const mongoose= require("mongoose");
 const app= express();
-let port= 8080;
+let port= 8880;
+const ObjectId= require("mongoose").ObjectId;
 
 const Listing= require('./models/listing.js')
 const main = require('./db.js');
@@ -9,6 +10,7 @@ const main = require('./db.js');
 app.set("view engine","ejs");
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
 
 
 app.set("views",'./views/listings')
@@ -21,12 +23,49 @@ app.get('/',(req,res)=>{
 
 
 
-app.get('/listings',(req,res)=>{
- let listing=Listing.find({}).then((data)=>{
-    res.render("index.ejs",{Listing});
- })
 
+// app.get('/listings', async (req,res)=>{
+//  let listing= await Listing.find({}).then((data)=>{
+//     res.render("index.ejs",{Listing});
+//  })
+
+// })
+app.get('/listings',async(req,res)=>{
+    let listing=await Listing.find({})
+   res.render("index.ejs",{listing});
+   
+   })
+
+   app.get('/listings/show/:id',async (req,res)=>{
+    let id= req.params.id;
+    //let mongoid= new ObjectId(id);
+    console.log(id);
+    let eachlisting= await Listing.find({_id:id})
+    console.log(eachlisting);
+    res.render("show.ejs",{eachlisting});
 })
+
+// adding new listing
+app.get('/listing/new',async (req,res)=>{
+    await res.render("newlisting.ejs");
+})
+
+app.post('/newlisting',async (req,res)=>{
+    let {title,description,image,price,location,country}=req.body;
+    let query={};
+    if(title && description&& image&&price&&location&&country){
+        query={title:title,description:description,image:image,price:price,location:location,country:country}
+        
+    }
+  let data= await Listing.insertOne(query,(err,data)=>{
+        if(err) throw err;
+        console.log(data)
+    });
+
+    
+})
+
+//update and edit data
 
 
 
