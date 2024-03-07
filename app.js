751f97d8fbe459d71c7092137ce8d078be3a9091
sachin <sachinjaing494@gmail.com>
2024-03-06 22:16:03 -0600

    crud

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express= require("express");
 const mongoose= require("mongoose");
 const app= express();
-let port= 8880;
+const path=require("path");
+let port= 8080;
 const ObjectId= require("mongoose").ObjectId;
 
 const Listing= require('./models/listing.js')
@@ -11,63 +12,64 @@ app.set("view engine","ejs");
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
-
-
-app.set("views",'./views/listings')
-
-
+app.set("views",'./views/listings');
 
 app.get('/',(req,res)=>{
     res.send("health is ok")
 })
 
+// showing all listing
 
-
-
-
-app.get('/listings',async(req,res)=>{
-    let listing=await Listing.find({})
-   res.render("index.ejs",{listing});
-   
-   })
-
-   app.get('/listings/show/:id',async (req,res)=>{
-    let id= req.params.id;
-    //let mongoid= new ObjectId(id);
-    console.log(id);
-    let eachlisting= await Listing.find({_id:id})
-    console.log(eachlisting);
-    res.render("show.ejs",{eachlisting});
-})
-
-// adding new listing
-app.get('/listing/new',async (req,res)=>{
-    await res.render("newlisting.ejs");
-})
-
-app.post('/newlisting', async (req, res) => {
+app.get('/listings', async (req, res) => {
     try {
-        let { title, description, image, price, location, country } = req.body;
-
-        const newlist = new Listing({
-            title: title,
-            description: description,
-            image: image,
-            price: price,
-            location: location,
-            country: country
-        });
-
-        await newlist.save();
-        res.render("index.ejs");
-    } catch (err) {
-        console.error('Error saving new listing:', err);
+        let listing = await Listing.find();
+        res.send(listing);
+    } catch (error) {
+        console.error(error);
         res.status(500).send('Internal Server Error');
     }
 });
 
 
-//update and edit data
+
+app.get('/listings/:id/details',async (req,res)=>{
+    let id= req.params.id;
+    console.log(id);
+    let listing=await Listing.findById(id);
+    console.log(listing);
+    res.render("show.ejs",{listing})
+    
+})
+
+
+
+// create new listing
+
+app.get('/listing/new',(req,res)=>{
+    res.render("newlisting.ejs");
+})
+
+
+app.post('/newlisting', async (req,res)=>{
+    let {title,description,price,location,country} = req.body;
+    const newlisting = new Listing({
+        title: title,  
+        description: description,  
+        price: Number(price),  
+        location: location,  
+        country: country  
+    });
+    newlisting.save();
+  
+   console.log(listing);
+})
+
+
+
+
+
+
+
 
 
 
