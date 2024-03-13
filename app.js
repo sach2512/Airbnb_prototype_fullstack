6fc3345d8f75d363e20281003c42a1da78dfb009
sachin <sachinjaing494@gmail.com>
2024-03-13 01:01:00 -0500

    crud

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express= require("express");
-const mongoose= require("mongoose");
-const url="mongodb://localhost:27017/sachin"
-const port=8180;
-const app=express();
+const express = require("express");
+const mongoose = require("mongoose");
+const url = "mongodb://localhost:27017/sachin"
+const port = 8080;
+const app = express();
 const methodOverride = require("method-override");
-const data= require('./data/data');
+const data = require('./data/data');
 const Listing = require("./models/listingmodel");
 app.set("view engine", "ejs");
 app.set("views", "./views/listings");
@@ -12,34 +12,34 @@ app.use(express.static('./public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-async function main(){
+async function main() {
     await mongoose.connect(url);
-   
-   }
-   
-   main()
-   .then(()=>{
-      console.log("Connection Successfull");
-   })
-   .catch(err => console.log(err));
-  
-   async function main() {
+
+}
+
+main()
+    .then(() => {
+        console.log("Connection Successfull");
+    })
+    .catch(err => console.log(err));
+
+async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/listing');
-  }
+}
 
 
-app.get('/listing', async (req,res)=>{
-    const listings = await Listing.find({});
+app.get('/listing', async (req, res) => {
+    const listings = await Listing.find();
     //console.log(listings);
-    res.render("index.ejs",{listings});
+    res.render("index.ejs", { listings });
 })
 
-app.get('/listing/details/:id',async (req,res)=>{
-    let id= req.params.id;
+app.get('/listing/details/:id', async (req, res) => {
+    let id = req.params.id;
     //console.log(id);
     const listing = await Listing.findById(id);
-    res.render("show.ejs",{listing});
-  
+    res.render("show.ejs", { listing });
+
 })
 
 
@@ -47,58 +47,65 @@ app.get('/listing/details/:id',async (req,res)=>{
 
 
 
-app.get('/listing/new',async (req,res)=>{
+app.get('/listing/new', async (req, res) => {
     await res.render("newlisting.ejs");
 })
 
-// app.post('/newlisting', async (req, res) => {
-//     try {
-//         let { title, description, image, price, location, country } = req.body;
+app.post('/newlisting', async (req, res) => {
+    try {
+        let { title, description, image, price, location, country } = req.body;
 
-//         const newlist = new Listing({
-//             title: title,
-//             description: description,
-//             image: image,
-//             price: price,
-//             location: location,
-//             country: country
-//         });
-
-//         await newlist.save();
-//        res.render("index.ejs");
-//     } catch (err) {
-//         console.error('Error saving new listing:', err);
-//         res.status(500).send('Internal Server Error');
-//     }
-// });
-app.post("/newlistings", async (req, res) => {
-    const newListing = new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listings");
-  });
+        const newlist = new Listing({
+            title: title,
+            description: description,
+            image: image,
+            price: price,
+            location: location,
+            country: country
+        });
+        await newlist.save();
+        res.redirect('/listing');
+    } catch (err) {
+        console.error('Error saving new listing:', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 
 
-app.get('/listing/:id/edit', async (req,res)=>{
-    let id= req.params.id;
-   let listing=  await Listing.findById(id);
-   res.render("edit.ejs",{listing});
-   console.log(listing);
+
+app.get('/listing/:id/edit', async (req, res) => {
+    let id = req.params.id;
+    let listing = await Listing.findById(id);
+    res.render("edit.ejs", { listing });
+    console.log(listing);
 })
 
-app.put('/listing/:id/edited',async(req,res)=>{
-    let id= req.params.id;
-    let {title,description,image,price,location,country}=req.body
-    await Listing.findByIdAndUpdate(id,{ ... req.body});
-        res.render("index.ejs");
-})
-// app.put("/listing/:id/edited", async (req, res) => {
-//     let { id } = req.params;
-//     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-//     res.send("edited")
-//   });
+app.put('/listing/:id/edited', async (req, res) => {
+    try {
+        let id = req.params.id;
+        let { title, description, image, price, location, country } = req.body;
+        
+        await Listing.findByIdAndUpdate(id, {
+            title: title,
+            description: description,
+            image: image,
+            price: price,
+            location: location,
+            country: country
+        });
+        
+        res.redirect(`/listing/details/${id}`);
+    } catch (error) {
+        // Handle errors
+        console.error("Error updating listing:", error);
+        res.status(500).send("Error updating listing");
+    }
+});
 
-app.listen(port,(err)=>{ 
-    if(err) throw err;
+
+
+app.listen(port, (err) => {
+    if (err) throw err;
     console.log("server is listening");
 })
